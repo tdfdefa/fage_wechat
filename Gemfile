e951a444951c43fad915ab1d1539aeb3494a45d3
tdf <test@tdf.com>
2016-01-29 17:55:18 +0800

    update

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+#source 'http://ruby.taobao.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
---
+++ app/controllers/wechats_controller.rb
@@ -0,0 +1,20 @@
+class WechatsController < ApplicationController
+	def home
+		if not valid_msg_signature(params)
+		      render text: "", status: 401
+		      return
+		   end
+		    content, status = Prpcrypt.decrypt(aes_key, params[:echostr], corp_id)
+		    render text: content, status: status
+	end
+
+	def valid_msg_signature(params)
+		timestamp         = params[:timestamp]
+		nonce             = params[:nonce]
+		echo_str          = params[:echostr]
+		msg_signature     = params[:msg_signature]
+		sort_params       = [qy_token, timestamp, nonce, echo_str].sort.join
+		current_signature = Digest::SHA1.hexdigest(sort_params)
+		current_signature == msg_signature
+    end
+end
