7e595aa03c3124df0eae9ac3284fcbe71d7a3cbc
tdf <test@tdf.com>
2016-02-15 13:05:44 +0800

    two

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,20 +1,28 @@
 class WechatsController < ApplicationController
 	def home
-		if valid_msg_signature(params)
-		      render text: "", status: 401
-		      return false
-		   end
+		#if valid_msg_signature(params)
+		    #  render text: "", status: 401
+		     # return false
+		  #end
 		   # content, status = Prpcrypt.decrypt(aes_key, params[:echostr], corp_id)
-		    render text: content, status: status
+		  #  render text: content, status: status
 	end
 
 	def valid_msg_signature(params)
+	
+		qy_token =  "token"
 		timestamp         = params[:timestamp]
 		nonce             = params[:nonce]
 		echo_str          = params[:echostr]
-		msg_signature     = params[:msg_signature]
-		sort_params       = [qy_token, timestamp, nonce, echo_str].sort.join
+		msg_signature     = params[:signature]
+		sort_params       = [qy_token, timestamp, nonce].sort.join
 		current_signature = Digest::SHA1.hexdigest(sort_params)
-		current_signature == msg_signature
+		if current_signature == msg_signature
+			return  true
+		else
+			return false
+		end
+
    	 end
+
 end
