5c5d966b8478c15978c18aa8ba7a06038c849695
tdf <test@tdf.com>
2016-02-16 10:55:52 +0800

    0216

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -2,32 +2,25 @@ class WechatsController < ApplicationController
  
 
 	def home
-		
-	do_GET	unless response
+	@@token ="token"
+	#if check_signature?(params[:signature],params[:timestamp],params[:nonce])  
+   	  return render text: params[:echostr]  
+    	#end  
+
 		  
 	end
 	#验证token
 	def do_GET (request, response)
     		response.status = 200
     		response.content_type = "text/plain"
-    		response.body = request.query["echostr"]     
+    		response.body = request.query["echostr"] 
  	end
-	def valid_msg_signature(params)
-	
-		qy_token =  "token"
-		timestamp         = params[:timestamp]
-		nonce             = params[:nonce]
-		echo_str          = params[:echostr]
-		msg_signature     = params[:signature]
-		sort_params       = [qy_token, timestamp, nonce].sort.join
-		current_signature = Digest::SHA1.hexdigest(sort_params)
-		if current_signature == msg_signature
-			return  true
-		else
-			return false
-		end
-
-   	 end
+ 	
+   	
+   	 private  
+	  def check_signature?(signature,timestamp,nonce)  
+	    Digest::SHA1.hexdigest([timestamp,nonce,@@token].sort.join) == signature  
+	  end  
 
 
 end
